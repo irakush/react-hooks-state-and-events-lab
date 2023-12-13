16e5440d5bdfe7c06a1b9e7d38b0af3b60ebd6d6
Igor Rakush <irakush@aol.com>
2023-12-13 14:18:32 -0500

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
 
+  const [isDarkMode, setIsDarkMode] = useState(false) // useState() => [ val, function]
+
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = isDarkMode ? "App dark" : "App light"
+
+  function handleToggleDarkMode() {
+    setIsDarkMode(!isDarkMode)
+  }
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleToggleDarkMode}>{isDarkMode ? "Light Mode" : "Dark Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
@@ -20,3 +26,6 @@ function App() {
 }
 
 export default App;
+
+
+// 1. import {useState} 
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setCategory] = useState("All")
+  // const [selectedCategory, setCategory] = useState(items)
+
+  function handleCategoryChange(e) {
+    setCategory(e.target.value)
+  }
+
+  if (selectedCategory != "All") {
+    items = items.filter((item) => {
+      return item.category === selectedCategory
+    })
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
